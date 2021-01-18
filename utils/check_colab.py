eba7b18e4520849fff1195ae1b8b43be2955c793
guiwitz <guiwitz@gmail.com>
2021-01-18 21:08:06 +0100

    alternative method to detect colab

--- utils/check_colab.py
+++ utils/check_colab.py
@@ -2,16 +2,12 @@ from google.colab import drive
 from pathlib import Path
 
 def set_datapath(default_path):
-    try:
-        # %tensorflow_version only exists in Colab.
-        %tensorflow_version 2.x
+    if 'google.colab' in str(get_ipython()):
         IS_COLAB = True
-    except Exception:
-        IS_COLAB = False
-        datapath = Path(default_path)
-        
-    if IS_COLAB:
         drive.mount('/gdrive')
         datapath = Path('/gdrive/My Drive')
+    else:
+        IS_COLAB = False
+        datapath = Path(default_path)
 
     return IS_COLAB
