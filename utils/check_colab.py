5852e5001ab8395e1a0c7b03c75240cff9806986
guiwitz <guiwitz@gmail.com>
2021-01-18 21:12:50 +0100

    check if already mounted

--- utils/check_colab.py
+++ utils/check_colab.py
@@ -4,10 +4,11 @@ from pathlib import Path
 def set_datapath(default_path):
     if 'google.colab' in str(get_ipython()):
         IS_COLAB = True
-        drive.mount('/gdrive')
+        if not os.path.isdir('/gdrive'):
+            drive.mount('/gdrive')
         datapath = Path('/gdrive/My Drive')
     else:
         IS_COLAB = False
         datapath = Path(default_path)
 
-    return IS_COLAB
+    return IS_COLAB, datapath
